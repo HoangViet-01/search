8d46b8667cd736664ea9f4c01f61ac754ce5c7e5
hoangbui2504 <24020507@vnu.edu.vn>
2026-09-24 22:42:24 +0700

    Hoan thien  BFS

--- search/search.py
+++ search/search.py
@@ -91,7 +91,7 @@ def depthFirstSearch(problem: SearchProblem):
 def breadthFirstSearch(problem: SearchProblem):
     """Search the shallowest nodes in the search tree first."""
 
-    queue = util.Queue()
+    queue = util.Queue() 
 
     start = problem.getStartState()
     queue.push((start, []))
